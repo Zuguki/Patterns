934af376cd96a4ba21a6eadf7606a62a69fff3ac
Zuguki <guskov.zahar20@gmail.com>
2021-10-16 17:17:26 -0400

    Multiply builder

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -8,11 +8,104 @@ namespace Builder
     {
         static void Main(string[] args)
         {
-            var builder = new HtmlBuilder("ul");
-            builder.AddChild("li", "Hello!")
-                .AddChild("li", "How r u?")
-                .AddChild("li", "...");
-            Console.WriteLine(builder.ToString());
+            //var builder = new HtmlBuilder("ul");
+            //builder.AddChild("li", "Hello!")
+            //    .AddChild("li", "How r u?")
+            //    .AddChild("li", "...");
+            //Console.WriteLine(builder.ToString());
+
+            var pb = new PersonBuilder();
+            Person person = pb
+                .Lives
+                    .At("145 London Road")
+                    .In("London")
+                    .WithPostcode("SW12BC")
+                .Works
+                    .At("Google")
+                    .AsA("Engineer")
+                    .Earning(1229990);
+
+            Console.WriteLine(person);
+        }
+    }
+
+    public class Person
+    {
+        public string StreetAddress, Postcode, City;
+        public string CompanyName, Position;
+        public int AnnualIncome;
+
+        public override string ToString()
+        {
+            return $"StreetAddress: {StreetAddress}\nPostcode: {Postcode}\nCity: {City}\n\n" +
+                   $"CompanyName: {CompanyName}\nPosition: {Position}\nAnnualIncome: {AnnualIncome}";
+        }
+    }
+
+    public class PersonBuilder
+    {
+        protected Person Person;
+
+        public PersonBuilder()
+        {
+            Person = new Person();
+        }
+        
+        public PersonBuilder(Person person)
+        {
+            Person = person;
+        }
+
+        public PersonAddressBuilder Lives => new PersonAddressBuilder(Person);
+        
+        public PersonJobBuilder Works => new PersonJobBuilder(Person);
+
+        public static implicit operator Person(PersonBuilder pb) => pb.Person;
+    }
+
+    public class PersonAddressBuilder : PersonBuilder
+    {
+        public PersonAddressBuilder(Person person) : base(person) { }
+
+        public PersonAddressBuilder At(string streetAddress)
+        {
+            Person.StreetAddress = streetAddress;
+            return this;
+        }
+
+        public PersonAddressBuilder In(string city)
+        {
+            Person.City = city;
+            return this;
+        }
+
+        public PersonAddressBuilder WithPostcode(string postCode)
+        {
+            Person.Postcode = postCode;
+            return this;
+        }
+    }
+
+    public class PersonJobBuilder : PersonBuilder
+    {
+        public PersonJobBuilder(Person person) : base(person) { }
+
+        public PersonJobBuilder At(string companyName)
+        {
+            Person.CompanyName = companyName;
+            return this;
+        }
+
+        public PersonJobBuilder AsA(string position)
+        {
+            Person.Position = position;
+            return this;
+        }
+
+        public PersonJobBuilder Earning(int annulIncome)
+        {
+            Person.AnnualIncome = annulIncome;
+            return this;
         }
     }
 
